8d7cb589dcae8e8b30d525fa4ed59c03fc0a52da
Thandeka-Makamba <thandekamakamba21@gmail.com>
2025-04-22 20:13:00 +0200

    Fixed project structure

--- src/inheritance/question1/Animal.java
+++ src/inheritance/question1/Animal.java
@@ -2,9 +2,33 @@ package inheritance.question1;
 
 /*
 
+QUESTION1
+
 Create a class called Animal with a method makeSound() that prints "Some generic animal sound".
 Then, create a class Dog that inherits from Animal and overrides makeSound() to print "Woof!".
 Instantiate both Animal and Dog, and call makeSound() on each.
+
+QUESTION 2
+
+Create a new class called Cat that extends Animal. Override the makeSound() method to print "Meow!".
+
+Now, in your Main class:
+
+Create an array of Animal objects with 3 elements: a Dog, a Cat, and a regular Animal.
+
+Loop through the array and call makeSound() on each element.
+
+QUESTION 3
+
+Add a method to the Dog class called fetch(), which prints "Dog is fetching the ball!".
+
+In the Main class, loop through the same array of Animal objects.
+
+For each object, call makeSound() as before.
+
+If the object is an instance of Dog, also call fetch().
+
+Use the instanceof keyword and casting to make this work.
 
  */
 
--- src/inheritance/question1/Main.java
+++ src/inheritance/question1/Main.java
@@ -2,10 +2,19 @@ package inheritance.question1;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Question1
         Animal animal = new Animal();
         Dog dog = new Dog();
-
         animal.makeSound();
         dog.makeSound();
+
+        //Question 2
+        Cat cat = new Cat();
+        Animal[] animals = {dog, cat, animal};
+
+        for (Animal obj: animals) {
+            obj.makeSound();
+        }
     }
 }
