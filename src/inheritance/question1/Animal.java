37ef9bf554392b9e5dfec2a3636fdec78cfb7092
Thandeka-Makamba <thandekamakamba21@gmail.com>
2025-04-22 19:20:54 +0200

    Solved inheritance question 1

---
+++ src/inheritance/question1/Animal.java
@@ -0,0 +1,16 @@
+package inheritance.question1;
+
+/*
+
+Create a class called Animal with a method makeSound() that prints "Some generic animal sound".
+Then, create a class Dog that inherits from Animal and overrides makeSound() to print "Woof!".
+Instantiate both Animal and Dog, and call makeSound() on each.
+
+ */
+
+public class Animal {
+
+    public void makeSound() {
+        System.out.println("Brrrrr");
+    }
+}
---
+++ src/inheritance/question1/Dog.java
@@ -0,0 +1,9 @@
+package inheritance.question1;
+
+public class Dog extends Animal{
+
+    @Override
+    public void makeSound() {
+        System.out.println("Woof!");
+    }
+}
