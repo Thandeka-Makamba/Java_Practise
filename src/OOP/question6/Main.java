72b17f73551154aa9e00b3950adb260143fc95fb
Thandeka-Makamba <thandekamakamba21@gmail.com>
2025-05-03 21:30:15 +0200

    Catching IllegalArgumentException

--- src/OOP/question6/Main.java
+++ src/OOP/question6/Main.java
@@ -6,19 +6,24 @@ public class Main {
         BankAccount checkingAccount = new CheckingAccount("Tha124", 3000.00);
 
         BankAccount[] accounts = {savingsAccount, checkingAccount};
+
         for (BankAccount account: accounts) {
-            if (account instanceof SavingsAccount) {
-                account.displayInfo();
-                account.deposit(200.00);
-                account.withdraw(500.00);
-                ((SavingsAccount) account).deductTax();
-                account.displayInfo();
-                System.out.println();
-            } else {
-                account.displayInfo();
-                account.deposit(1000.00);
-                account.withdraw(2000.00);
-                account.displayInfo();
+            try {
+                if (account instanceof SavingsAccount) {
+                    account.displayInfo();
+                    account.deposit(200.00);
+                    account.withdraw(5000.00);
+                    ((SavingsAccount) account).deductTax();
+                    account.displayInfo();
+                    System.out.println();
+                } else {
+                    account.displayInfo();
+                    account.deposit(1000.00);
+                    account.withdraw(2000.00);
+                    account.displayInfo();
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error in account: " + account.getAccountNumber() + ", Error: " + e.getMessage());
             }
         }
     }
