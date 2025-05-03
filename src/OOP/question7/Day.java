5166081e3dd3c23764b0e95e8693462f4583535f
Thandeka-Makamba <thandekamakamba21@gmail.com>
2025-05-03 23:58:36 +0200

    Solution to OOP question 7 - Enums

---
+++ src/OOP/question7/Day.java
@@ -0,0 +1,9 @@
+package OOP.question7;
+
+public enum Day {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+
+    public boolean isWeekend() {
+        return this == SATURDAY || this == SUNDAY;
+    }
+}
