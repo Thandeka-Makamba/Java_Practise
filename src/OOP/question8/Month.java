8c9815f3c0bcd897d7b95c29c5244a6692764d78
Thandeka-Makamba <thandekamakamba21@gmail.com>
2025-05-04 07:11:33 +0200

    Solution to OOP question 8 - Enums

---
+++ src/OOP/question8/Month.java
@@ -0,0 +1,28 @@
+package OOP.question8;
+
+public enum Month {
+    JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
+
+    public String getSeason() {
+
+        switch (this) {
+            case DECEMBER:
+            case JANUARY:
+            case FEBRUARY:
+                return "Winter";
+            case MARCH:
+            case APRIL:
+            case MAY:
+                return "Spring";
+            case JUNE:
+            case JULY:
+            case AUGUST:
+                return "Summer";
+            case SEPTEMBER:
+            case OCTOBER:
+            case NOVEMBER:
+                return "Fall";
+        }
+        return "";
+    }
+}
